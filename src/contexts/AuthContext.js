16f6943dbaa5d6ce6be66554252bccd5d1293bcb
KrishKrosh <krish.life21@gmail.com>
2021-11-09 16:22:29 -0500

    final auth fixes - please nobody change it

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,11 @@
 import React, { useContext, useState, useEffect } from "react";
-import app, {auth} from "../firebase";
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-
+import app, { auth } from "../firebase";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const AuthContext = React.createContext();
 
@@ -13,12 +17,13 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
 
-  function signup(email, password) {
+  function signup(auth, email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
-  function login(email, password) {
-    return signInWithEmailAndPassword(email, password);
+  function login(auth, email, password) {
+    console.log("login");
+    return signInWithEmailAndPassword(auth, email, password);
   }
 
   function logout() {
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,33 +1,39 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 // import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 //firebase stuff
 import { useAuth } from "../contexts/AuthContext";
 import { db } from "../firebase";
 import { getFirestore, collection, setDoc, doc } from "firebase/firestore";
 //import state things
-import { useState } from 'react';
+import { useState } from "react";
+import { getAuth } from "firebase/auth";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -35,7 +41,8 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignInSide() {
-  const {signup}  = useAuth();
+  const auth = getAuth();
+  const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -44,14 +51,12 @@ export default function SignInSide() {
     setLoading(true);
     event.preventDefault();
     try {
-
       const data = new FormData(event.currentTarget);
 
-      await signup(data.get('email'), data.get('password'));
-      await setDoc(doc(db, "users", data.get('email')),{
-
-        name: data.get('name'),
-        email: data.get('email'),
+      await signup(auth, data.get("email"), data.get("password"));
+      await setDoc(doc(db, "users", data.get("email")), {
+        name: data.get("name"),
+        email: data.get("email"),
       })
         .then((docRef) => {
           console.log("Document written with ID: ");
@@ -60,17 +65,17 @@ export default function SignInSide() {
           console.error("Error adding document: ", error);
           setError(error);
         });
-        //redirect to "/Task"
-        window.location.href = "/tasks";
+      //redirect to "/Task"
+      window.location.href = "/tasks";
     } catch (err) {
       console.log(err);
       setError("Error Signing Up: " + err);
     }
-  };
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -78,12 +83,11 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(lake.jpeg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              "#C1C2AD",
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            backgroundImage: "url(lake.jpeg)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) => "#C1C2AD",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -91,21 +95,29 @@ export default function SignInSide() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: '#535E4B' }}>
+            <Avatar sx={{ m: 1, bgcolor: "#535E4B" }}>
               {/* <LockOutlinedIcon /> */}
             </Avatar>
-            <Typography component="h1" variant="h5"
-            sx={{ m: 1, color: "#535E4B" }}>
+            <Typography
+              component="h1"
+              variant="h5"
+              sx={{ m: 1, color: "#535E4B" }}
+            >
               Sign Up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
-              sx={{ m: 1, color: "#535E4B" }}
+                sx={{ m: 1, color: "#535E4B" }}
                 margin="normal"
                 required
                 fullWidth
@@ -116,7 +128,7 @@ export default function SignInSide() {
                 autoFocus
               />
               <TextField
-              sx={{ m: 1, color: "#535E4B" }}
+                sx={{ m: 1, color: "#535E4B" }}
                 margin="normal"
                 required
                 fullWidth
@@ -127,7 +139,7 @@ export default function SignInSide() {
                 autoFocus
               />
               <TextField
-              sx={{ m: 1, color: "#535E4B" }}
+                sx={{ m: 1, color: "#535E4B" }}
                 margin="normal"
                 required
                 fullWidth
@@ -146,9 +158,7 @@ export default function SignInSide() {
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2,
-                  backgroundColor: (t) =>
-                  "#535E4B"}}
+                sx={{ mt: 3, mb: 2, backgroundColor: (t) => "#535E4B" }}
               >
                 Sign Up
               </Button>
